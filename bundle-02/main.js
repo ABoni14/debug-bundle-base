96b7d34c2f5829a2b8f75474fca0cb0c6389d29a
ABoni14 <aboni150@gmail.com>
2021-12-09 16:20:47 +0100

    bundle_-02

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -9,11 +9,11 @@
 ​
 *******************************************************************************/
 
-
 // ESERCIZIO 1
 function checkAge() {
     const myAge = 32;
-    const message = '';
+    let message = '';
+    // message è una variabile, non una costante
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
@@ -22,23 +22,27 @@ function checkAge() {
     }
 }
 checkAge();
+// in questa funzione viene presa l'età, se è inferiore a 18 viene mandato un messaggio, sennò un altro
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
+// è sbagliata la grammatica della parola "length"
 printColorsNumber();
 
 
 // ESERCIZIO 3
 function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
+    const userNumber = parseInt(prompt('Inserisci un numero'));
     const total = userNumber + 12;
 
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+// aggiungo parseInt per accertarmi di ricevere un numero e non una stringa
+// questa funzione chiede un numero all'utente, aggiunge 12 e restituisce la somma finale
 
 
 // ESERCIZIO 4
@@ -46,10 +50,10 @@ function checkAccess() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+    let grantAccess = false;
 
     if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
+        grantAccess = true;
     }
 
     if (grantAccess === true) {
@@ -59,6 +63,8 @@ function checkAccess() {
     }
 }
 checkAccess();
+// I primi due valori sono booleani, non sono stringhe, questo valore non richiede gli apici. 
+// Chiede di inserire la propria email con un prompt, se questa email è presente nell'array di email, viene consentito l'accesso, sennò no
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -75,7 +81,7 @@ function checkAccessImproved() {
       if (userEmail.length > 5) {
             
         if (email === userEmail) {
-        grantAccess = 'true';            
+        grantAccess = true;            
             
       }
         
@@ -86,34 +92,6 @@ function checkAccessImproved() {
     } else {
         console.log('Accesso negato!');
     }
-}
+}}
 checkAccessImproved();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// mancava una parentesi graffa di chiusura, vanno tolti gli apici per il valore booleano
